19147a9530da8d0b891d925b1c16b3681f3b9689
saramohey <saramohey42@gmail.com>
2019-06-24 04:57:18 +0200

    update commit

--- app/src/main/java/com/gradpro/home_automation/smarthomeautomation/changeusername.java
+++ app/src/main/java/com/gradpro/home_automation/smarthomeautomation/changeusername.java
@@ -3,11 +3,11 @@ package com.gradpro.home_automation.smarthomeautomation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class navigation extends AppCompatActivity {
+public class changeusername extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation);
+        setContentView(R.layout.activity_changeusername);
     }
 }
