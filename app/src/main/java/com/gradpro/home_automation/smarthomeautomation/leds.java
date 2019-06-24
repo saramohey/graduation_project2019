b3667a60424a4e9004c0d30bc7be1cfe366113b6
saramohey <saramohey42@gmail.com>
2019-06-24 14:16:33 +0200

    update commit

--- app/src/main/java/com/gradpro/home_automation/smarthomeautomation/leds.java
+++ app/src/main/java/com/gradpro/home_automation/smarthomeautomation/leds.java
@@ -7,35 +7,63 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class leds extends AppCompatActivity {
 
 
+    FirebaseDatabase database=FirebaseDatabase.getInstance();
+    ToggleButton ledtoggle;
+    DatabaseReference reff=FirebaseDatabase.getInstance().getReference();
+    DatabaseReference ledref=reff.child("Leds");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leds);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ledtoggle= (ToggleButton) findViewById(R.id.toggleleds);
         setSupportActionBar(toolbar);
-        /*FirebaseDatabase database=FirebaseDatabase.getInstance();
-        DatabaseReference reff =database.getReference("Leds");
-        ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-              if (isChecked) {
-               //   val=1
-                reff.setValue("1");
-               } else {
-                    //val=0
-                 reff.setValue("0");
+    }
 
+    @Override
+    protected void onStart ()
+    {
+
+        super.onStart();
+        ledref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String val = dataSnapshot.getValue(String.class);
+
+                if(val.equals("1")) ledtoggle.setChecked(true);
+                else    ledtoggle.setChecked(false);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        ledtoggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    ledref.setValue("1");
+                } else {
+                   ledref.setValue("0");
                 }
             }
-        });*/
+        });
 
     }
 }
+
+
+
--- app/src/main/java/com/gradpro/home_automation/smarthomeautomation/lock.java
+++ app/src/main/java/com/gradpro/home_automation/smarthomeautomation/lock.java
@@ -3,16 +3,87 @@ package com.gradpro.home_automation.smarthomeautomation;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class lock extends AppCompatActivity {
 
+    FirebaseDatabase database=FirebaseDatabase.getInstance();
+    ToggleButton wintog;
+    ToggleButton dotog;
+    DatabaseReference lreff=FirebaseDatabase.getInstance().getReference().child("Locks");
+    DatabaseReference winref=lreff.child("Windows");
+    DatabaseReference doref=lreff.child("Doors");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        wintog= (ToggleButton) findViewById(R.id.windowtoggle);
+        dotog= (ToggleButton) findViewById(R.id.doortoggle);
         setSupportActionBar(toolbar);
 
     }
-}
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        winref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    String valw = dataSnapshot.getValue(String.class);
+
+                    if(valw.equals("1")) wintog.setChecked(true);
+                    else    wintog.setChecked(false);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            wintog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        winref.setValue("1");
+                    } else {
+                        winref.setValue("0");
+                    }
+                }
+            });
+            doref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    String vald = dataSnapshot.getValue(String.class);
+
+                    if(vald.equals("1")) dotog.setChecked(true);
+                    else    dotog.setChecked(false);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            dotog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        doref.setValue("1");
+                    } else {
+                        doref.setValue("0");
+                    }
+                }
+            });
+
+        }
+    }
+
